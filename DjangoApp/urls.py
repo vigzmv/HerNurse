a03ba2176ae6e40856c02a677aa173f614b0064e
vigzmv <vigzmv@outlook.com>
2016-10-15 20:12:36 +0530

    Views

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
 from App.forms import LoginForm
-from App.views import register
+from App import views as App_v
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('App.urls')),
     url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
     url(r'^logout/$', views.logout, {'next_page': '/login'}),
-    url(r'^register/$', register, name="register"),
+    url(r'^register/$', App_v.register, name="register"),
+    url(r'^password/$', App_v.change_password, name='change_password'),
 ]
