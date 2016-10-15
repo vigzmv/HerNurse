1c7fa298f6c6b9e4d592ce95a21601c99002ea32
Hiten Sharma <sharmahiten3@gmail.com>
2016-10-15 20:12:07 +0530

    Models Defined

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(ModelUser)
+admin.site.register(ModelDiscuss)
+admin.site.register(ModelTag)
+admin.site.register(ModelQuestion)
+admin.site.register(ModelAnswer)
